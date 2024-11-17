6f071fafa2c09653d69e2b99a1f70b3ff5f9e548
monaem <61699407+monaemhmila@users.noreply.github.com>
2024-11-17 23:29:28 +0100

    abitan
    abitan

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
-const port = 5692
+const port = 5693
 require('dotenv').config()
 
 const fs = require('fs');
@@ -37,6 +37,6 @@ const server = app.listen(port, () => {
 
 // Connect to MongoDB
 const DATABASE_URL = process.env.DB_URL || 'mongodb+srv://hamilamonem:ccavhKglmUrq6kd7@cluster0.2u6rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE = process.env.DB || 'keller'
+const DATABASE = process.env.DB || 'abitan'
 
 db(DATABASE_URL, DATABASE);
