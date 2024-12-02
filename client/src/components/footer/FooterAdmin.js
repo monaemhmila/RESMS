4dd5efb070aed19fe414e02172c70c8d3138ec51
monaem <61699407+monaemhmila@users.noreply.github.com>
2024-12-02 23:10:47 +0100

    test
    test

--- client/src/components/footer/FooterAdmin.js
+++ client/src/components/footer/FooterAdmin.js
@@ -42,7 +42,7 @@ export default function Footer() {
             fontWeight='500'
             color={textColor}
             target="_blank"
-            href='https://www.jemsit.com/#/home'> Jemsit.</Link> Need help, facing issues, or looking for a new feature? Contact us for paid support and services at <Link href='mailto:alternatecrm@gmail.com' fontWeight='500'
+            href='https://www.jemsit.com/#/home'> Jemsit.</Link> Need help, facing issues, or looking for a new feature? Contact us for support and services at <Link href='mailto:alternatecrm@gmail.com' fontWeight='500'
               color={textColor}
               target="_blank">jemsit.contact@gmail.com</Link>
         </Text>
